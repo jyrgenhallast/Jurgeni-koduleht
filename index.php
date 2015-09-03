a048cff3e4fbb50a5433f1799965edacac5a9ed1
alvarpungar <alvar.pungar@khk.ee>
2015-09-03 16:39:17 +0300

    Esilehele lisada pilt [Delivers #102651162]

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>Jürgen Hallast</title>
 </head>
 <body>
-    <h1 style="color:navy ; text-align:center">Jürgen Hallast</div>
+    <h1 style="color:navy ; text-align:center">Jürgen Hallast</h1>
+    <img src="assets/img/JH.jpg">
 </body>
 </html>
