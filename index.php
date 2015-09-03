7b442a84dd5c477d2974c2112dfc2f7154efb614
unknown <alvarpungar@khk.ee>
2015-09-03 16:04:35 +0300

    Esilehele lisada lehe pealkiri [Delivers #102575914]

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>J�rgeni koduleht</title>
+</head>
+<body>
+    <h1 style="color:navy ; text-align:center">Jürgen Hallast</div>
+</body>
+</html>
