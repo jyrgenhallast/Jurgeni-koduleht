fb7066c46313f1f488bc4195548f85c5b4388755
alvarpungar <alvar.pungar@khk.ee>
2015-09-08 11:04:09 +0300

    Esilehele lisada pilt [Delivers #102651162]

--- index.php
+++ index.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Jürgen Hallast</title>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 </head>
 <body>
-    <h1 style="color:navy ; text-align:center">Jürgen Hallast</h1>
+    <h1>Jürgen Hallast</h1>
+    <hr>
     <img src="assets/img/JH.jpg">
+        <div>
+        <table>
+            <!--siia tuleb informatsioon-->
+            <th colspan="2"> Informatsioon </th>
+                <tr>
+                    <td>Sünniaeg</td>
+                    <td>20.04.1983</td>
+                </tr>
+                <tr>
+                    <td>Elukoht</td>
+                    <td>Tartus</td>
+                </tr>
+                <tr>
+                    <td>Varasemad õpingud</td>
+                    <td>Tartu Kommertsgümnaasium, EMÜ, Tartu KHK</td>
+                </tr>
+                <tr>
+                    <td>Hobid</td>
+                    <td>Jalgrattad, reisimine</td>
+                </tr>
+        </table>
+        </div>
 </body>
 </html>
